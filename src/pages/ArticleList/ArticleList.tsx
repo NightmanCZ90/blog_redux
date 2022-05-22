a13a435086f60128ed9556e08c37294621222da1
NightmanCZ90 <novak.jan90@gmail.com>
2022-05-22 17:20:15 +0200

    feat: Render basic articles data

--- src/pages/ArticleList/ArticleList.tsx
+++ src/pages/ArticleList/ArticleList.tsx
@@ -1,7 +1,36 @@
-const ArticleList: React.FC = (props) => {
+import './ArticleList.scss';
+import { ArticleWithDates } from '../../types/articles';
+import { useAppSelector } from '../../store';
+
+const ArticleList: React.FC = () => {
+  const { articles } = useAppSelector((state) => state.articleList);
+
   return (
-    <div>ArticleList</div>
+    <div className="article-list">
+
+      <div className="article-list__header">
+        <h1>Recent articles</h1>
+      </div>
+
+      <div className="article-list__content">
+        {renderArticles(articles)}
+      </div>
+    </div>
   )
+
+  function renderArticles(articles: ArticleWithDates[]) {
+    if (articles.length === 0) {
+      return (
+        <div>
+          <h3>No results</h3>
+        </div>
+      )
+    }
+
+    return articles.map(article => (
+      <div key={article.articleId}>{article.title}</div>
+    ))
+  }
 }
 
 export default ArticleList;
