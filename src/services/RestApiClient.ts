9fcb936dec5819a2ca73c7b9961aeeb398fc5e5a
NightmanCZ90 <novak.jan90@gmail.com>
2022-05-22 21:55:38 +0200

    data: Add auth API requests

--- src/services/RestApiClient.ts
+++ src/services/RestApiClient.ts
@@ -1,13 +1,40 @@
 import { ArticleList } from '../types/articles';
+import { AccessToken, Tenant } from '../types/user';
 import ApiClient from './ApiClient';
 
 class RestApiClient extends ApiClient {
 
   /**
+   * Authentication
+   */
+
+  async signUp(body: {
+    name: string,
+    password: string,
+  }) {
+    return this.axiosRequest<Tenant>({
+      url: '/tenants',
+      method: 'POST',
+      body,
+    })
+  }
+
+  async login(body: {
+    username: string,
+    password: string,
+  }) {
+    return this.axiosRequest<AccessToken>({
+      url: '/login',
+      method: 'POST',
+      body,
+    })
+  }
+
+  /**
    * Articles
    */
 
-  async getArticles() {
+   async getArticles() {
     return this.axiosRequest<ArticleList>({
       url: '/articles',
       method: 'GET',
