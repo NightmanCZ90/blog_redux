0807eeac42f54c6827c81461cbaab20d1196a651
NightmanCZ90 <novak.jan90@gmail.com>
2022-05-23 21:29:17 +0200

    feat: Add boilerplate for MyArticles page

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Navigation from './components/Navigation';
 import About from './pages/About';
 import ArticleList from './pages/ArticleList';
 import Login from './pages/Login';
+import MyArticles from './pages/MyArticles';
 import SignUp from './pages/SignUp';
 
 const App: React.FC = () => {
@@ -14,6 +15,7 @@ const App: React.FC = () => {
       <Routes>
         <Route path="/" element={<ArticleList />} />
         <Route path="/about" element={<About />} />
+        <Route path="/my-articles" element={<MyArticles />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/login" element={<Login />} />
       </Routes>
---
+++ src/pages/MyArticles/MyArticles.tsx
@@ -0,0 +1,11 @@
+import './MyArticles.scss';
+
+const MyArticles: React.FC = () => {
+  return (
+    <div className="my-articles">
+      My Articles
+    </div>
+  )
+}
+
+export default MyArticles;
