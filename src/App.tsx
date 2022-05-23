8c9e75bd202edc3c2df9d76e2d692fbac6f0e989
NightmanCZ90 <novak.jan90@gmail.com>
2022-05-23 22:42:09 +0200

    feat: Add route protection

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 
 import './App.scss';
 import Navigation from './components/Navigation';
@@ -7,15 +7,35 @@ import ArticleList from './pages/ArticleList';
 import Login from './pages/Login';
 import MyArticles from './pages/MyArticles';
 import SignUp from './pages/SignUp';
+import { useAppSelector } from './store';
+import { Tenant } from './types/user';
+
+const Protected = (props: { user: Tenant | null, children: React.ReactElement }) => {
+  const { children, user } = props;
+
+  if (!user) {
+    return <Navigate to="/" replace />
+  }
+  return children;
+}
 
 const App: React.FC = () => {
+  const { user } = useAppSelector(state => state.currentUser);
+
   return (
     <div className="App">
       <Navigation />
       <Routes>
         <Route path="/" element={<ArticleList />} />
         <Route path="/about" element={<About />} />
-        <Route path="/my-articles" element={<MyArticles />} />
+        <Route
+          path="/my-articles"
+          element={
+            <Protected user={user}>
+              <MyArticles />
+            </Protected>
+          } 
+        />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/login" element={<Login />} />
       </Routes>
